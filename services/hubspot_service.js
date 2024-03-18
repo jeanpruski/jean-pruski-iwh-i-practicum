cfcf8a6b3e295e49ff93d0e11c8ba6455299bad6
Jean Pruski <44072294+jeanpruski@users.noreply.github.com>
2024-03-18 19:04:20 +0100

    express server without style

---
+++ services/hubspot_service.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const hubspot_service = {
+  hubspotHeaders: {
+    headers: {
+      'Authorization': `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+    }
+  },
+  getObjects: async (objectId) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectId}?limit=100&archived=false&properties=name&properties=first_skill&properties=secondary_skill`;
+
+    try {
+      const response = await axios.get(url, hubspot_service.hubspotHeaders);
+      return response.data.results;
+    } catch (error) {
+      return;
+    }
+  },
+  updateObjects: async (objectId, formData) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectId}`;
+
+    try {
+      const item = {
+          properties: {
+              "name": formData.name,
+              "first_skill": formData.first_skill,
+              "secondary_skill": formData.secondary_skill
+          }
+      };
+     
+      const response = await axios.post(url, item, hubspot_service.hubspotHeaders);
+      return {
+        result: true
+      }
+    } catch (error) {
+      return {
+        result: false,
+        error
+      }
+    }
+  }
+}
+
+module.exports = hubspot_service;
---
+++ services/tools_service.js
@@ -0,0 +1,11 @@
+
+const tools_service = {
+  page_names: {
+    home: 'HSP | Home',
+    update_get: 'HSP | Update',
+    update_post: '',
+  },
+  page_title: 'Super Heros !'
+}
+
+module.exports = tools_service;
